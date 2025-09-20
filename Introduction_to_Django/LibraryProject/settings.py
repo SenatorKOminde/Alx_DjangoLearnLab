0c63a2fe10526d7fd36e1e9a6393519462c26a68
SenatorKOminde <sonrollwriters@gmail.com>
2025-09-20 21:28:27 +0300

    push

--- Introduction_to_Django/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/settings.py
@@ -118,7 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Use custom user model
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
